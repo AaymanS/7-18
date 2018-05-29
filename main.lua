aaf88ccced6f634ab9b0d68aac3d147d6ef0a0e3
AaymanS <36240729+AaymanS@users.noreply.github.com>
2018-05-29 17:56:35 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,12 @@
+-----------------------------------------------------------------------------------------
+-- Created by: Aayman Shameem
+-- Created on: May 29, 2018
+-- 
+-- This code will take the user to splash scene
+-----------------------------------------------------------------------------------------
 
+local composer = require( "composer" )
+
+display.setStatusBar( display.HiddenStatusBar )
+
+composer.gotoScene( "scene.splashScene" )
